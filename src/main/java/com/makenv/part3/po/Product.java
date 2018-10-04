ac5f7e4cac0821de15f636d47a85df64f8bd6873
erlie <lilimin@social-touch.com>
2018-10-04 12:45:08 +0800

    增加part3，学习源码写的示例

---
+++ src/main/java/com/makenv/part3/po/Product.java
@@ -0,0 +1,32 @@
+package com.makenv.part3.po;
+
+public class Product {
+
+    private long id;
+    private String productName;
+    private double price;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
